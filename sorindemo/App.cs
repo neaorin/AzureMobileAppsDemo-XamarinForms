f9acdc7d31c8481130d1e05018f315f6bbf0b3c7
Administrator <Administrator@DENISACHETEST>
2016-05-05 18:58:45 +0300

    Full Name reading from AAD

--- sorindemo/App.cs
+++ sorindemo/App.cs
@@ -62,7 +62,7 @@ namespace sorindemo
                 case MobileServiceAuthenticationProvider.Google: user.FullName = userdata["google"]["user_id"].ToString(); break;
                 case MobileServiceAuthenticationProvider.MicrosoftAccount: user.FullName = userdata["microsoftaccount"]["user_id"].ToString(); break;
                 case MobileServiceAuthenticationProvider.Twitter: user.FullName = userdata["twitter"]["user_id"].ToString(); break;
-                case MobileServiceAuthenticationProvider.WindowsAzureActiveDirectory: user.FullName = userdata["aad"]["user_id"].ToString(); break;
+                case MobileServiceAuthenticationProvider.WindowsAzureActiveDirectory: user.FullName = $"{userdata["aad"]["claims"]["givenname"].ToString()} {userdata["aad"]["claims"]["surname"].ToString()}"; break;
             }
             return user;
         }
