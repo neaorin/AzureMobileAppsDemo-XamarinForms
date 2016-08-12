acdc98f66e607d8e941247633c775c6c84eb3375
sorinpe <sorinpe@SORINPEYOGA>
2016-08-13 01:32:32 +0300

    iOS server auth flow

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading.Tasks;
 using Foundation;
+using Microsoft.WindowsAzure.MobileServices;
 using UIKit;
 
 namespace sorindemo.iOS
 {
 	[Register ("AppDelegate")]
-	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IAuthenticate
 	{
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+        public async Task<MobileServiceUser> Authenticate(MobileServiceAuthenticationProvider provider)
+        {
+            var user = await TodoItemManager.DefaultManager.CurrentClient.LoginAsync(UIApplication.SharedApplication.KeyWindow.RootViewController, provider);
+            return user;
+        }
+
+        public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init ();
 
